b9fef69e6dbd368e5a72085666f7c76e8ddaf927
m1nhnguy3n <minhdacamst@gmail.com>
2024-03-20 11:54:34 +0700

    feat:ex6-create-update-page: update link

--- js/list.js
+++ js/list.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const pageNumber = 1;
     const perPageNumber = 12;
     const renderPosts = async (page, perPage) => {
-        let uri = `http://localhost:3000/posts?_page=${page}&_per_page=${perPage}`;
+        let uri = `https://blog-server-y5zy.onrender.com/posts?_page=${page}&_per_page=${perPage}`;
         const res = await fetch(uri);
         const data = await res.json();
         renderGrid(data.data);
