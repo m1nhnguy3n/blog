4302b7a990d218dfb7fae11372d2b16c174d77f8
m1nhnguy3n <minhdacamst@gmail.com>
2024-03-20 11:36:07 +0700

    feat:ex6-create-update-page

--- js/list.js
+++ js/list.js
@@ -1,25 +1,21 @@
-
 document.addEventListener('DOMContentLoaded', function () {
     const container = document.querySelector('.grid-container');
     const paginationContainer = document.querySelector('.pagination');
-    console.log(paginationContainer)
+    console.log(paginationContainer);
     const pageNumber = 1;
     const perPageNumber = 12;
     const renderPosts = async (page, perPage) => {
-        let uri = `https://blog-server-y5zy.onrender.com/posts?_page=${page}&_per_page=${perPage}`;
+        let uri = `http://localhost:3000/posts?_page=${page}&_per_page=${perPage}`;
         const res = await fetch(uri);
         const data = await res.json();
-        console.log(data)
-        renderGrid(data.data)
-        pagination(data)
-
-
-    }
-
+        console.log(data);
+        renderGrid(data.data);
+        pagination(data);
+    };
 
     const renderGrid = (data) => {
         let template = '';
-        data?.forEach(item => {
+        data?.forEach((item) => {
             template += `
             <div class="grid-item">
                 <img class="card-img-top" src=${item?.image} alt="${item?.title}">
@@ -31,56 +27,68 @@ document.addEventListener('DOMContentLoaded', function () {
                     <a href="/detailPage.html?blog_id=${item.id}" class="btn btn-primary">Detail</a>
                 </div>
             </div>
-          `
-        })
+          `;
+        });
 
         container.innerHTML = template;
         const deleteButtons = document.querySelectorAll('.btn-delete-post');
 
-        deleteButtons.forEach(button => {
+        deleteButtons.forEach((button) => {
             button.addEventListener('click', () => {
-                const modal = document.getElementById('id01')
-                modal.style.display = 'block'
-                const yes = document.getElementById('yes')
-                const no = document.getElementById('no')
-                yes.addEventListener('click', () => {
-                    modal.style.display = 'none'
+                const modal = document.getElementById('id01');
+                modal.style.display = 'block';
+                const yes = document.getElementById('yes');
+                const no = document.getElementById('no');
+                yes.addEventListener('click', async () => {
                     const postId = button.getAttribute('data-id');
-                    const filteredData = data?.filter(item => item.id !== postId);
-                    renderGrid(filteredData)
-                })
+                    
+                    await fetch(
+                        `https://blog-server-y5zy.onrender.com/posts/${postId}`,
+                        {
+                            method: 'DELETE',
+                        }
+                    )
+                        .then((res) => {
+                            if (res.status === 200) {
+                                modal.style.display = 'none';
+                                const filteredData = data?.filter(
+                                    (item) => item.id !== postId
+                                );
+                                renderGrid(filteredData);
+                            }
+                        })
+                        .catch((error) => console.log(error));
+
+                });
                 no.addEventListener('click', () => {
-                    modal.style.display = 'none'
-                })
+                    modal.style.display = 'none';
+                });
             });
         });
-    }
+    };
 
     const pagination = (data) => {
-        console.log(data)
+        console.log(data);
         let template = '';
         for (let i = 1; i <= data.pages; i++) {
             const isActive = data.prev + 1 === i ? 'active' : '';
             template += `
             <li class="page-item ${isActive}"><a class="page-link" href="#" data-id="${i}">${i}</a></li>
-          `
+          `;
         }
-        paginationContainer.innerHTML = template
+        paginationContainer.innerHTML = template;
         const pageItems = document.querySelectorAll('.page-item');
-        pageItems.forEach(item => {
+        pageItems.forEach((item) => {
             item.addEventListener('click', () => {
-                const page = parseInt(item.querySelector('.page-link').getAttribute('data-id'));
-                renderPosts(page, 6)
+                const page = parseInt(
+                    item.querySelector('.page-link').getAttribute('data-id')
+                );
+                renderPosts(page, 6);
             });
         });
-    }
-
+    };
 
     window.addEventListener('DOMContentLoaded', () => {
         renderPosts(1, 6);
     });
-
 });
-
-
-
