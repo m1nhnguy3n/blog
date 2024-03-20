55dc96d93aa3e860e367034e22ec8daa2ef535b5
m1nhnguy3n <minhdacamst@gmail.com>
2024-03-20 15:55:22 +0700

    fix comment

--- js/detail_blog.js
+++ js/detail_blog.js
@@ -59,13 +59,13 @@ document.addEventListener('DOMContentLoaded', function () {
                 <div class="form-row">
                     <div class="form-group col-md-4">
                         <label for="author">Author</label>
-                            ${item.comments.map((comment, index) => `
+                            ${item.comments?.map((comment, index) => `
                                 <input readonly class="form-control mt-1" value="${comment.author_comment}">
                             `).join('')}
                     </div>
                     <div class="form-group col-md-8">
                         <label for="comment">Comment</label>
-                            ${item.comments.map((comment, index) => `
+                            ${item.comments?.map((comment, index) => `
                                 <input readonly class="form-control mt-1" value="${comment.text}">
                             `).join('')}
                     </div>
